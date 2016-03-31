181e10bc0b23b62128c884cafadae704346da5cd
Brett <bfrable@redventures.com>
2016-03-30 22:28:09 -0400

    updates

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var angular;
+
 var app = angular.module('portfolio', ['ui.router']);
 
 app.controller('mainCtrl', function($scope, $timeout){
@@ -18,7 +20,7 @@ app.controller('mainCtrl', function($scope, $timeout){
 
 app.controller('workCtrl', function($scope, $http) {
 
-    $http.get("./scripts/projects.json").then(function(response) {
+    $http.get('./scripts/projects.json').then(function(response) {
         $scope.projects = response.data;
     });
 
@@ -26,33 +28,33 @@ app.controller('workCtrl', function($scope, $http) {
 
 app.config(function($stateProvider, $urlRouterProvider){
 
-    $urlRouterProvider.otherwise("/about")
+    $urlRouterProvider.otherwise('/about');
 
     $stateProvider
         .state('about', {
-            url: "/about",
-            templateUrl: "./views/about.html"
+            url: '/about',
+            templateUrl: './views/about.html'
         })
         .state('work', {
-            url: "/work",
-            templateUrl: "./views/work.html"
+            url: '/work',
+            templateUrl: './views/work.html'
         })
         .state('contact', {
-            url: "/contact",
-            templateUrl: "./views/contact.html"
-        })
+            url: '/contact',
+            templateUrl: './views/contact.html'
+        });
 });
 
 app.directive('mainNav', function(){
     return {
         templateUrl: './partials/nav.html',
         replace: true
-    }
+    };
 });
 
 app.directive('loader', function(){
     return {
         templateUrl: './partials/loader.html',
         replace: true
-    }
+    };
 });
